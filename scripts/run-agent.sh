bd7c56e33e8de11eb4851d6a52d81ec006129181
Ivan Shubin <ivan.ishubin@gmail.com>
2012-06-04 16:31:16 +0200

    .

--- scripts/run-agent.sh
+++ scripts/run-agent.sh
@@ -1,1 +1,1 @@
-java -jar oculus-grid.jar net.mindengine.oculus.grid.Main agent $* 
+java -jar oculus-grid.jar agent $* 
--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -1,1 +1,1 @@
-java -jar oculus-grid.jar net.mindengine.oculus.grid.Main server $* 
+java -jar oculus-grid.jar server $* 
