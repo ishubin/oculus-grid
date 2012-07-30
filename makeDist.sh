681bb8d0b4a7cd803ee27b93882912c4ee0a3e9c
Ivan Shubin <ivan.ishubin@gmail.com>
2012-07-30 16:44:13 +0200

    changed deployment scripts

--- makeDist.sh
+++ makeDist.sh
@@ -1,6 +1,9 @@
 set -e
 
-cd ..
+versionInPom=`cat pom.xml | grep "<version>.*</version>" | head -n 1 | sed 's/[<version> | </version>]//g'`
+version=`cat pom.xml | grep "<version>.*</version>" | head -n 1 | sed 's/[<version> | </version> | (SNAPSHOT) | -]//g'`
+echo Version is $version
+
 mkdir -p bin/storage
 mkdir -p target
 rm -rf bin/*
@@ -13,7 +16,7 @@ cp scripts/run-server.sh bin/.
 cp scripts/run-agent.sh bin/.
 
 cd bin
-zip -r -9 oculus-grid.zip *
+zip -r -9 oculus-grid-$version.zip *
 
 ls -1 | grep -v *.zip | xargs rm -rf 
 
